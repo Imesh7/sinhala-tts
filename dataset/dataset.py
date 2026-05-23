b2dc79c4e208aa0e9724031ea61ac6d39f51a353
imesh <2019imesh@gmail.com>
2026-05-23 21:52:55 +0530

    fix : added dataset filter & remove trim audio pre & post noise

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -7,6 +7,7 @@ import torchaudio
 from torch.utils.data import Dataset
 
 from utils import mel_spectrogram
+import tqdm
 
 
 class TTSDataset(Dataset):
@@ -19,33 +20,80 @@ class TTSDataset(Dataset):
         hop_length: int = 256,
         n_fft: int = 1024,
     ):
-        self.root = Path(data_path)
-        self.meta = pd.read_csv(
-            self.root / "dataset.csv",
+        self.data_path = Path(data_path)
+        self.wav_dir = self.data_path / "audios"
+        self.metadata_path = self.data_path / "dataset_clean.csv"
+        self.tokenizer = tokenizer
+        self.sample_rate = sample_rate
+
+        MAX_FRAMES_FILTER = 1200 # max audio size is 12 seconds
+
+        # Load metadata (adjust format to your actual CSV)
+        self.metadata = pd.read_csv(
+            self.metadata_path,
             sep=",",
             header=0,
             names=["sentence", "audio"],
-            engine="python",
+            engine='python',
         )
-        self.tok = tokenizer
-        self.sample_rate = sample_rate
-        self.mel = mel_spectrogram(
-            n_mels=n_mels, hop_length=hop_length, n_fft=n_fft, sample_rate=sample_rate
-        )
-        self.rs = None
+        
+        # Path("/kaggle/working/length_cache.csv") or
+        length_cache_path = Path("/teamspace/studios/this_studio/length_cache.csv")
+        
+        # This is compute frames of the audio files to remove long audios that consume more vram
+        if length_cache_path.exists():
+            lengths_df = pd.read_csv(length_cache_path)
+            self.metadata["frames"] = lengths_df["frames"]
+        else:
+            print("⚠️ Computing lengths (one-time cost)...")
 
-        self.mel_mean = (
-            -1.7977
-        )  # These values are dataset-specific and should be computed from the training data
+            frames = []
+            for path in tqdm(self.metadata["audio"]):
+                wav_path = self.wav_dir / path.replace("\\", "/")
+                # print(wav_path)
+                try:
+                    print(wav_path)
+                    y, _ = librosa.load(wav_path, sr=self.sample_rate)
+                    frames.append(len(y) // hop_length)
+                except:
+                    print("librosa read error")
+                    frames.append(0)
+
+            self.metadata["frames"] = frames
+
+            pd.DataFrame({"frames": frames}).to_csv(length_cache_path, index=False)
+            print("✅ Saved length cache")
+
+        # before = len(self.metadata)
+        self.metadata = self.metadata[self.metadata["frames"] <= MAX_FRAMES_FILTER]
+
+        self.mel_transform = torchaudio.transforms.MelSpectrogram(
+            sample_rate=sample_rate,
+            n_fft=n_fft,
+            hop_length=hop_length,
+            n_mels=n_mels,
+            power=1.0,
+            normalized=False,
+            center=True,
+        )
+        # Resampler will be created on-demand
+        self.resampler = None
+
+        self.mel_mean = -1.7977
         self.mel_std = 2.0155
 
     def __len__(self):
         return len(self.meta)
 
     def __getitem__(self, idx):
-        row = self.meta.iloc[idx]
-        text_tokens = self.tok(row["sentence"]).input_ids
-        wav_path = self.root / row["audio"].replace("\\", "/")
+        row = self.metadata.iloc[idx]
+
+        # 1. Tokenize text
+        text = row["sentence"]
+        text_tokens = self.tokenizer(text).input_ids
+
+        wav_filename = row["audio"]
+        wav_path = self.wav_dir / wav_filename.replace("\\", "/")
         wav, sr = librosa.load(str(wav_path), sr=None, mono=True)
         w = torch.from_numpy(wav).float()
 
@@ -55,10 +103,10 @@ class TTSDataset(Dataset):
             w = w.mean(dim=0, keepdim=True)
 
         if sr != self.sample_rate:
-            if self.rs is None or getattr(self.rs, "orig_freq", None) != sr:
-                self.rs = torchaudio.transforms.Resample(sr, self.sample_rate)
-            w = self.rs(w)
-
+            if self.resampler is None or getattr(self.resampler, "orig_freq", None) != sr:
+                self.resampler = torchaudio.transforms.Resample(sr, self.sample_rate)
+            w = self.resampler(w)
+        
         # Trim silence
         wav_np = w.squeeze(0).numpy()
         trimmed_np, _ = librosa.effects.trim(
@@ -71,10 +119,15 @@ class TTSDataset(Dataset):
 
         w = w / (w.abs().max() + 1e-8)
 
-        mel = self.mel(w)
+        mel = self.mel_transform(w)
         mel = torch.clamp(mel, min=1e-7).log().squeeze(0)
-
+        
         # Mel normalize
         mel = (mel - self.mel_mean) / self.mel_std
 
-        return {"text_tokens": text_tokens, "mel_spec": mel, "text": row["sentence"]}
+        return {
+            "text_tokens": text_tokens,
+            "mel_spec": mel,
+            "text": text,
+            "filename": wav_filename,
+        }
